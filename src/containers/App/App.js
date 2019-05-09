45edcc1ec79c95f6108ba1871c62554c06951c49
swolecoder <swolecoder@gmail.com>
2019-05-09 12:50:28 -0400

    first project

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import styles from './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <h2>React App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
